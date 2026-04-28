1ab2c114f51de51c32be5443c879dd9db5e02e59
Christian Farias <cbradford158@gmail.com>
2026-04-27 22:36:21 -0400

    Added Starscape! Next Animation *gulp*

--- jawn.js
+++ jawn.js
@@ -4,20 +4,31 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
-function draw() {
+function draw1() {
 
   const canvas=document.getElementById("canvas-1");
   const ctx=canvas.getContext("2d");
+  const rect =canvas.getBoundingClientRect();
+
+  horz=rect.width;
+  vert=rect.height;
+
+  console.log(horz);
+  console.log(vert);
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
 
 
-  for (let i=0; i<40;i++){
 
-    ctx.fillStyle="rgb(209, 19, 19)";
+  for (let i=0; i<60;i++){
+
+    ctx.fillStyle="rgb(255, 255, 255)";
 
     ctx.beginPath();
-    const x = getRandomInt(0,300);
-    const y= getRandomInt(0,150);
-    const radius=getRandomInt(0,3);
+    const x = getRandomInt(0,horz);
+    const y= getRandomInt(0,vert);
+    const radius=getRandomInt(1,4)/2;
 
     ctx.arc(x,y,radius,0,Math.PI * 2, true);
 
@@ -25,4 +36,51 @@ function draw() {
   }
 }
 
-draw();
+function draw2() {
+
+  const canvas=document.getElementById("canvas-2");
+  const ctx=canvas.getContext("2d");
+  const rect =canvas.getBoundingClientRect();
+
+  horz=rect.width;
+  vert=rect.height;
+
+  console.log(horz);
+  console.log(vert);
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+
+
+  for (let i=0; i<60;i++){
+
+    ctx.fillStyle="rgb(255, 255, 255)";
+
+    ctx.beginPath();
+    const x = getRandomInt(0,horz);
+    const y= getRandomInt(0,vert);
+    const radius=getRandomInt(1,4)/2;
+
+    ctx.arc(x,y,radius,0,Math.PI * 2, true);
+
+    ctx.fill();
+  }
+}
+
+draw1();
+draw2();
+
+// Project Stardust
+// No for loops I'll have to use setInterval() timeout and Animation Frames
+// 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
