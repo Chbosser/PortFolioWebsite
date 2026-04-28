25de99ff2c0a543720e1803710faf9abf6296e47
Christian Farias <cbradford158@gmail.com>
2026-04-27 22:54:27 -0400

    Increased star radius

--- jawn.js
+++ jawn.js
@@ -18,7 +18,7 @@ function setupStarCanvas(canvasId, starCount = 60) {
       stars.push({
         x: getRandom(0, canvas.width),
         y: getRandom(0, canvas.height),
-        radius: getRandom(0.5, 1.8),
+        radius: getRandom(0.5, 2),
 
         alpha: getRandom(0.3, 1),
         speed: getRandom(0.009, 0.02),
